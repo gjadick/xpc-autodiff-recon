ff13f3be8c5935a8a69e04675af68e3d817e1d58
gjadick <giavanna@uchicago.edu>
2026-04-10 11:49:15 +0200

    update inputs

--- src/xpc/xscatter.py
+++ src/xpc/xscatter.py
@@ -12,6 +12,7 @@ X-ray optics helper functions and other things (e.g. material compositions).
 """
 
 import numpy as np
+import jax.numpy as jnp
 import xraydb
 
 
@@ -62,6 +63,75 @@ def get_energy(wavelen):
         pass  
     return 1e-3*h*c / (wavelen*J_eV)
 
+
+def gaussian2D(x, y, fwhm, normalize=True):
+    """
+    Generate a 2D Gaussian kernel.
+    x, y : 1D arrays
+        Grid coordinates [arbitrary length]
+    fwhm : float
+        Full-width at half-max of the Gaussian (units must match x,y)
+    normalize: bool (default True)
+        If True, normalize the kernel to sum to 1
+    """
+    sigma = fwhm / (2 * jnp.sqrt(2 * jnp.log(2)))
+    X, Y = jnp.meshgrid(x, y)
+    kernel = jnp.exp(-(X**2 + Y**2) / (2 * sigma**2))
+    if normalize:
+        kernel = kernel / jnp.sum(kernel)
+    return kernel
+
+    
+def lorentzian2D(x, y, fwhm, normalize=True):
+    """
+    Generate a 2D Lorentzian kernel.
+    x, y : 1D arrays
+        Grid coordinates [arbitrary length]
+    fwhm : float
+        Full-width at half-max of the Lorentzian (units must match x,y)
+    normalize: bool (default True)
+        If True, normalize the kernel to sum to 1
+    """
+    gamma = fwhm/2
+    X, Y = jnp.meshgrid(x, y)
+    kernel = gamma / (2 * PI * (X**2 + Y**2 + gamma**2)**1.5)
+    if normalize:
+        kernel = kernel / jnp.sum(kernel)
+    return kernel
+
+
+def convolve2d_diy(img, kernel):
+    """Convolve two 2D arrays with scipy-like mode='same'."""
+    H, W = img.shape
+    KH, KW = kernel.shape
+    F = jnp.fft.rfft2(img, s=(H+KH-1, W+KW-1))
+    G = jnp.fft.rfft2(kernel, s=(H+KH-1, W+KW-1))
+    out = jnp.fft.irfft2(F * G, s=(H+KH-1, W+KW-1))
+    oh, ow = (KH-1)//2, (KW-1)//2   # center crop (mode='same')
+    return out[oh:oh+H, ow:ow+W]
+
+    
+def apply_psf(img, dx, fwhm=None, kernel_width=6.0, psf='lorentzian'):
+    if fwhm is None:
+        return img
+
+    psf = psf.lower()
+    assert psf in ('lorentzian', 'gaussian')
+
+    half_width = kernel_width * fwhm        
+    x = jnp.arange(-half_width, half_width + dx, dx)
+
+    if psf == 'lorentzian':
+        kernel = lorentzian2D(x, x, fwhm)
+    else:
+        kernel = gaussian2D(x, x, fwhm)
+
+    img_pad = jnp.pad(img, kernel.shape, constant_values=img[0,0])    # pad img to account for fillvalue = 0. Corner [0,0] pixel temp
+    img_nonideal_pad = convolve2d_diy(img_pad, kernel)
+    img_nonideal = img_nonideal_pad[kernel.shape[0]:-kernel.shape[0], kernel.shape[1]:-kernel.shape[1]]
+        
+    return img_nonideal
+    
 
 class Material:
 
